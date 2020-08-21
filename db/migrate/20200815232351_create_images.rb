fc384726d0eb470da7d1164b77d280dcc59373b0
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-08-20 20:34:38 -0400

    added db, seed

--- db/migrate/20200815232351_create_images.rb
+++ db/migrate/20200815232351_create_images.rb
@@ -2,7 +2,8 @@ class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
       t.string :name
-
+      t.string :image
+      t.string :location
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Image.destroy_all
+
+
+Image.create(id: 1, name: 'Al Ahsa', image: '/assets/alAhsa.png', location: 'Saudi Arabia')
+Image.create(id: 2, name: 'Antarctica', image: '/assets/antarctica.png', location: 'Antarctica')
+Image.create(id: 3, name: 'Arequipa', image: '/assets/Arequipa.png', location: 'Peru')
+Image.create(id: 4, name: 'Camptonville', image: '/assets/Camptonville.png', location: 'United States')
+Image.create(id: 5, name: 'Capitan Prat', image: '/assets/capitanPrat.png', location: 'Chile')
+Image.create(id: 6, name: 'Chicago', image: '/assets/chicago.png', location: 'United States')
+Image.create(id: 7, name: 'Dashoguz Province', image: '/assets/dashoguz.png', location: 'Turkmenistan')
+Image.create(id: 8, name: 'Eustaquio Mendez', image: '/assets/eustaquio.png', location: 'Bolivia')
+Image.create(id: 9, name: 'Fremont', image: '/assets/fremont.png', location: 'United States')
+Image.create(id: 10, name: 'Hail Province', image: '/assets/hailProvince.png', location: 'Saudi Arabia')
+
