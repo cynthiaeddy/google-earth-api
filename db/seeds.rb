4887f7fcbe89774bb08d3818b96abbc4631bb6c1
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-08-22 18:11:24 -0400

    resized photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,35 @@
 Image.destroy_all
 
 
-Image.create(id: 1, name: 'Al Ahsa', image: '/assets/alAhsa.png', location: 'Saudi Arabia')
-Image.create(id: 2, name: 'Antarctica', image: '/assets/antarctica.png', location: 'Antarctica')
-Image.create(id: 3, name: 'Arequipa', image: '/assets/Arequipa.png', location: 'Peru')
-Image.create(id: 4, name: 'Camptonville', image: '/assets/Camptonville.png', location: 'United States')
-Image.create(id: 5, name: 'Capitan Prat', image: '/assets/capitanPrat.png', location: 'Chile')
-Image.create(id: 6, name: 'Chicago', image: '/assets/chicago.png', location: 'United States')
-Image.create(id: 7, name: 'Dashoguz Province', image: '/assets/dashoguz.png', location: 'Turkmenistan')
-Image.create(id: 8, name: 'Eustaquio Mendez', image: '/assets/eustaquio.png', location: 'Bolivia')
-Image.create(id: 9, name: 'Fremont', image: '/assets/fremont.png', location: 'United States')
-Image.create(id: 10, name: 'Hail Province', image: '/assets/hailProvince.png', location: 'Saudi Arabia')
+Image.create(id: 1, name: 'Al Ahsa', image: 'https://imgur.com/421MLqe.png', location: 'Saudi Arabia')
+Image.create(id: 3, name: 'Arequipa', image: 'https://imgur.com/iJZFRyi.png', location: 'Peru')
+Image.create(id: 7, name: 'Dashoguz Province', image: 'https://imgur.com/LAXdyNa.png', location: 'Turkmenistan')
+Image.create(id: 8, name: 'Eustaquio Mendez', image: 'https://imgur.com/Bwyhaq6.png', location: 'Bolivia')
+Image.create(id: 9, name: 'Fremont', image: 'https://imgur.com/iYj7KZ4.png', location: 'United States')
+Image.create(id: 10, name: 'Hail Province', image: 'https://imgur.com/YQeT0aE.png', location: 'Saudi Arabia')
+
+
+
+Image.create(id: 11, name: 'Huaraz', image: 'https://imgur.com/ZLyHL0h.png', location: 'Peru')
+Image.create(id: 12, name: 'Kufra District', image: 'https://imgur.com/elCdzKX.png', location: 'Libya')
+
+Image.create(id: 15, name: 'Inyo County', image: 'https://imgur.com/QK6hILk.png', location: 'United States')
+Image.create(id: 16, name: 'Kajalleq', image: 'https://imgur.com/m5625AE.png', location: 'Greenland')
+Image.create(id: 17, name: 'La Chorrera', image: 'https://imgur.com/9dmVGQZ.png', location: 'Panama')
+Image.create(id: 19, name: 'Mangystau Region', image: 'https://imgur.com/ldiqwW0.png', location: 'Kazakhstan')
+Image.create(id: 20, name: 'Maralinga', image: 'https://imgur.com/2mPMDZg.png', location: 'Australia')
+Image.create(id: 21, name: 'Mill Atole', image: 'https://imgur.com/Y13KBxD.png', location: 'Marshall Islands')
+Image.create(id: 22, name: 'Mulega Municipality', image: 'https://imgur.com/pTyXNnP.png', location: 'Mexico')
+Image.create(id: 23, name: 'Mysove m', image: 'https://imgur.com/CaOiE4c.png', location: 'Crimea')
+Image.create(id: 24, name: 'North Slope', image: 'https://imgur.com/a5UiKgR.png', location: 'United States')
+Image.create(id: 25, name: 'St. Clair County', image: 'https://imgur.com/Xbqs3lh.png', location: 'United States')
+Image.create(id: 26, name: 'Taylor County', image: 'https://imgur.com/8yVmR7C.png', location: 'United States')
+Image.create(id: 27, name: 'Tiba', image: 'https://imgur.com/CdOVw01.png', location: 'Spain')
+Image.create(id: 28, name: 'Tooele County', image: 'https://imgur.com/DzDcoRu.png', location: 'United States')
+Image.create(id: 29, name: 'Utersum', image: 'https://imgur.com/R7Ph4q2.png', location: 'Germany')
+Image.create(id: 30, name: 'Vegaoyan', image: 'https://imgur.com/vNap6kU.png', location: 'Norway')
+Image.create(id: 31, name: 'Wiluna', image: 'https://imgur.com/ZwbJMfl.png', location: 'Australia')
+Image.create(id: 32, name: 'Wintabyina', image: 'https://imgur.com/ssy3lJj.png', location: 'Australia')
+Image.create(id: 33, name: 'Kura', image: 'https://imgur.com/AGAyUZe.png', location: 'Somaila')
+
 
